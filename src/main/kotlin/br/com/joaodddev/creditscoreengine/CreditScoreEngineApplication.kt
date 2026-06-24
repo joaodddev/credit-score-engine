1ac3ab02e74a903665490434cb253250f6991a0e
João Victor <joaodddev@gmail.com>
2026-06-24 08:30:21 -0300

    feat: initial project scaffold

---
+++ src/main/kotlin/br/com/joaodddev/creditscoreengine/CreditScoreEngineApplication.kt
@@ -0,0 +1,11 @@
+package br.com.joaodddev.creditscoreengine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditScoreEngineApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditScoreEngineApplication>(*args)
+}
